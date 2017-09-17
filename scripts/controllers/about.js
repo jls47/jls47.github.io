a89d1299548ee9a4f6c0ea523794601411389751
John Luke Schaefer <lukeschaefer@gmail.com>
2017-09-16 20:24:09 -0700

    Built jls47githubio from commit d56852b on branch master

---
+++ scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jls47githubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jls47githubioApp
+ */
+angular.module('jls47githubioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jls47githubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jls47githubioApp
+ */
+angular.module('jls47githubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
